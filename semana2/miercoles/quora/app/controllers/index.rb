039a4f38abe2fb31db296ac9531cea83368e592c
codeamac06 <codea_mac_06@CodeaCamp06s-Mac-mini.local>
2017-02-24 16:11:36 -0600

    Avance 2 de Quora

--- semana2/miercoles/quora/app/controllers/index.rb
+++ semana2/miercoles/quora/app/controllers/index.rb
@@ -102,9 +102,14 @@ def create
     @user = User.new(params[:user])
     @user.username = params[:username]
     @user.email = params[:user_email]
+    if params[:user_password] == params[:confirm_user_password]
     @user.password = params[:user_password]
-    if user.save!
-      redirect to("/#{@user.id}")
+    end
+
+    if @user.save!
+      @user = User.find_by(email: params[:user_email])
+      session[:user_id] = "#{@user.id}"
+      redirect to("/users/#{@user.id}")
     else
       erb :register_account
     end
